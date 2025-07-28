2e250eac05721b607745d49c05a06333bc1c91fb
Kevin Yugla <kiyugla@espe.edu.ec>
2025-07-28 13:49:18 -0500

    Agregados estados del formulario y datos de ejemplo para tabla y lista

--- src/App.js
+++ src/App.js
@@ -26,16 +26,39 @@ function App() {
     setPaginaActual(nuevaPagina);
   };
 
+  // Datos para el formulario
+  const [datosUsuario, setDatosUsuario] = useState({
+    nombre: '',
+    email: '',
+    password: '',
+    telefono: '',
+    website: '',
+    buscar: '',
+  });
+
+  const [errores, setErrores] = useState({});
+
+  // Info de la tabla
+  const columnas = ['ID', 'Nombre', 'Email', 'Puntos'];
+  const filas = [
+    [1, 'Ippo Makunouchi', 'ippo@boxeo.com', 89],
+    [2, 'Takamura Mamoru', 'takamura@boxeo.com', 156],
+    [3, 'Miyata Ichiro', 'miyata@boxeo.com', 67],
+    [4, 'Sendo Takeshi', 'sendo@boxeo.com', 78],
+  ];
+
+  const listaTecnicas = [
+    'Jab directo',
+    'Cross de poder',
+    'Hook lateral',
+    'Uppercut',
+    'Esquiva lateral',
+    'Bloqueo alto',
+  ];
+
   return (
     <div>
-      <Navbar seccionActiva={paginaActual} cambiarSeccion={irAPagina} />
-
-      <div
-        className="container py-4"
-        style={{ backgroundColor: 'rgb(48, 48, 46)', minHeight: '100vh' }}
-      >
-        {/* Aquí irán las páginas */}
-      </div>
+      <p>App en construcción...</p>
     </div>
   );
 }
